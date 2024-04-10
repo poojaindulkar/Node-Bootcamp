dc8ffefb0758edb684af56c93bbf177ba01302be
Pooja Indulkar <poojaindulkar200@gmail.com>
2024-04-10 18:48:25 +0530

    delete user

--- 3.advance_concepts/routes/index.js
+++ 3.advance_concepts/routes/index.js
@@ -8,4 +8,36 @@ router.get('/', function(req, res, next) {
   // res.send("Hello");
 });
 
+
+router.get("/create", async function(req,res){
+  const createuser=await userModel.create({
+    username: "Neha",
+    name:"Neha",
+    age:"17"
+  })
+  
+  // res.send(`User Created Succesfully !!!! ${createuser}`)
+  res.send(createuser)
+})
+
+router.get("/alluser",async function(req,res){
+  const alluser=await userModel.find(); //! find() returns ans in array
+  res.send(alluser)
+})
+
+router.get("/oneuser", async function(req,res){
+  const user=await userModel.findOne({age:17});
+  //if user with specific condition is not present it returns null
+  // console.log(user);
+  res.send(user);
+})
+
+
+router.get("/delete",async function(req,res){
+  let deleteUser= await userModel.findOneAndDelete({
+    username:"Pooja"
+  });
+  res.send(deleteUser);
+})
+
 module.exports = router;
